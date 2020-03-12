e177cc6d048146087013edd91e4260709b05b5fd
bruce <bruceworkzone@outlook>
2020-03-12 22:43:54 +0800

    整理目录

--- .history/src/pages/index_20200312224302.tsx
+++ .history/src/pages/index_20200312224302.tsx
@@ -4,12 +4,7 @@ import Link from 'umi/link';
 export default function() {
   return (
     <div className={styles.normal}>
-      <div>css样式demo集合</div>
-      <ul>
-        <li>
-          <Link to="/sticky-shadow">吸附头和单侧阴影</Link>
-        </li>
-      </ul>
+      <Link to="/sticky-shadow">吸附头和单侧阴影</Link>
     </div>
   );
 }
--- .history/src/pages/index_20200312224322.tsx
+++ .history/src/pages/index_20200312224322.tsx
@@ -4,7 +4,6 @@ import Link from 'umi/link';
 export default function() {
   return (
     <div className={styles.normal}>
-      <div>css样式demo集合</div>
       <ul>
         <li>
           <Link to="/sticky-shadow">吸附头和单侧阴影</Link>
